f8fe7a4f5b119d27989163d95ea67db396d847cb
unknown <macjay314@gmail.com>
2020-07-07 11:07:04 -0700

    add test for 'postListReducer' expect to fail.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Cat Chat!</h1>
+    </React.Fragment>
+  )
+}
+
+export default Header;
---
+++ src/components/PostControl.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import NewPostForm from './NewPostForm';
+import PostList from './PostList';
